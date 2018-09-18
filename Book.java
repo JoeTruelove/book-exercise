67d60602a5e32c87f8d79154322f7da71f17751c
JoeTruelove <43154676+JoeTruelove@users.noreply.github.com>
2018-09-17 21:23:50 -0400

    83-90
    working on 90

--- Book.java
+++ Book.java
@@ -3,24 +3,83 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @Author Joseph Truelove
+ * @version 9/17/2018
  */
 class Book
 {
     // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String refNumber;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
+        refNumber = "";
     }
 
     // Add the methods here ...
+    public String getAuthor()
+    {
+        return author;   
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public void printAuthor()
+    {
+        System.out.println("The Author's name is " + author + ".");
+        System.out.println();
+    }
+    public void printTitle()
+    {
+        System.out.println("The title of the book is " + title + ".");
+        System.out.println();
+    }
+    public int getPages()
+    {
+        return pages;
+    }
+    public void printDetails()
+    {
+        if (refNumber.length() > 0)
+    {
+        if (pages > 1)
+        System.out.println(author + " is the author of the book: " + title +
+        ", which contains " + pages + " pages in the book. Reference Number: " + refNumber);
+        else
+        System.out.println(author + " is the author of the book: " + title +
+        ", which contains " + pages + " page in the book Reference Number: " + refNumber);
+    }
+    else
+    if (pages > 1)
+        System.out.println(author + " is the author of the book: " + title +
+        ", which contains " + pages + " pages in the book.");
+        else
+        System.out.println(author + " is the author of the book: " + title +
+        ", which contains " + pages + " page in the book");
+    }
+    public void setRefNumber(String ref)
+    {
+        while(refNumber.length() < 2)
+        {
+            System.out.println("Error must be more than 2");
+            refNumber = ref;
+        }
+        refNumber = ref;
+        
+    }
+    public String getRefNumber()
+    {
+        return refNumber;
+    }
 }
---
+++ __SHELL22.java
@@ -0,0 +1,11 @@
+
+public class __SHELL22 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("D:\\School Apps\\GitHub\\book-exercise");
+final Book book1 = (Book)__bluej_runtime_scope.get("book1");
+
+
+java.lang.String __bluej_param0 = "z";
+book1.setRefNumber(__bluej_param0);
+
+}}
